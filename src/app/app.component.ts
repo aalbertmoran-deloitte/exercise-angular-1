f5ca54148bbeb26b0c17ebc77bc643302b32d8ee
AlanAlbertMn <aalbertmoran@deloitte.com>
2024-02-23 17:54:45 -0600

    Added styles and tabs for each entity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { StudentComponent } from "./student/student.component";
 import { ProjectComponent } from './project/project.component';
 import { CourseComponent } from './course/course.component';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, StudentComponent, ProjectComponent, CourseComponent]
+    imports: [CommonModule, MatTabsModule, RouterOutlet, StudentComponent, ProjectComponent, CourseComponent]
 })
 
 export class AppComponent {}
